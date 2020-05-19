85ed2505fd9f618d1472edcc6508f6e6a94e2b1d
DickensJuma <dickensjuma13@gmail.com>
2020-05-19 09:21:49 +0300

    add booble sort method using while and each properties

---
+++ while_loop.rb
@@ -0,0 +1,11 @@
+def booble_sort(arr)
+    arr.each do
+    j=0
+    while j < arr.length-1
+        arr[j],arr[j+1]=arr[j+1],arr[j] if arr[j]>arr[j+1]
+        j+=1
+    end
+end
+p arr
+end
+ booble_sort([3,4,7,5,9,1,2])
